99d0f18cab0efbfad9dde7774e1b5bf91d0dfe41
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2024-03-23 16:00:10 +0530

    data fetching hook - 1st done

---
+++ Data_fetching_hooks/src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function useTodos(n = 5) {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const value = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      });
+    }, n * 1000);
+
+    axios.get("https://sum-server.100x.devs.com/todos").then((res) => {
+      setTodos(res.data.todos);
+      setLoading(false);
+    });
+
+    return () => {
+      clearInterval(value);
+    };
+  }, [n]);
+  return { todos, loading };
+}
+
+function App() {
+  const { todos, loading } = useTodos(5);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      {todos.map((todo) => (
+        <Track todo={todo} />
+      ))}
+    </>
+  );
+}
+
+function Track({ todo }) {
+  return (
+    <div>
+      {todo.title}
+      <br />
+      {todo.description}
+    </div>
+  );
+}
+
+export default App;
